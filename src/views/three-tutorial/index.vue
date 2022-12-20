1dc081ee1118302c1fbc72384dfe46041f5da058
loopzhou <363415642@qq.com>
2022-12-20 11:07:34 +0800

    feat(three): raycaster tutorial

--- src/views/three-tutorial/index.vue
+++ src/views/three-tutorial/index.vue
@@ -4,14 +4,11 @@
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
-// import Base from './01-base-three';
-// import SpotLightExample from './02-spot-light-shadow';
-
-// const base = new SpotLightExample();
-
 // http://localhost:4000/#/three-tutorial/01-base-three
 // http://localhost:4000/#/three-tutorial/02-spot-light-shadow
 // http://localhost:4000/#/three-tutorial/03-gui
+// http://localhost:4000/#/three-tutorial/04-instance-raycaster
+
 const { example = '01-base-three' } = useRoute().params;
 const exampleFile = await import(`./${example}`);
 new exampleFile.default();
